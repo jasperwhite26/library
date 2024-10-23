a70251f1d1da8e29aff94f53091b91b5649e4f98
jasper <whiteana001@warren.k12.in.us>
2024-10-23 13:59:36 -0400

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+
+        Library A = new Library();
+        A.name = "A";
+        A.addBooks(100);
+        System.out.println("Library A added: " + Library.getTotalBooks());
+
+        Library B = new Library();
+        B.name = "B";
+        B.addBooks(150);
+        System.out.println("Total books: " + Library.getTotalBooks());
+    }
 }
